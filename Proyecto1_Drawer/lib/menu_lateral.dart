7d55cfa6078b06bbd06926af775c9a4a00a0d1f4
jpaccas075 <a_jorge.pacheco.castro@iespablopicasso.es>
2024-10-04 17:55:21 +0200

    Añado Proyecto1_Drawer completo

---
+++ Proyecto1_Drawer/lib/menu_lateral.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:drawer/screens/name_screen.dart';
+import 'package:drawer/screens/row_photos_screen.dart';
+import 'package:drawer/screens/column_photos_screen.dart';
+import 'package:drawer/screens/icons_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Proyecto Drawer'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero, // Elimina el padding predeterminado
+          children: <Widget>[
+            const UserAccountsDrawerHeader(
+              accountName: Text("Software Society Inc."),
+              accountEmail: Text("jpaccas075@g.educaand.es"),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://st2.depositphotos.com/1718692/5917/i/450/depositphotos_59179351-stock-photo-pine-forest-near-the-mountain.jpg"),
+                      fit: BoxFit
+                          .cover)), // Ajusta la imagen para cubrir todo el espacio
+            ),
+            Ink(
+              color: Colors.indigo[400],
+              child: ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text('Nombre y Apellidos'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NameScreen()),
+                  );
+                },
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.photo),
+              title: const Text('Fotos en Fila'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const RowPhotosScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.photo_album),
+              title: const Text('Fotos en Columna'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ColumnPhotosScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.star),
+              title: const Text('Iconos'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const IconsScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text('Selecciona una opción del menú'),
+      ),
+    );
+  }
+}
